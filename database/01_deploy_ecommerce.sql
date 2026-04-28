fd3c5cbb7342a4fb54e8fc2f45f5053a84673104
Douglas Silva <douglasitpro@gmail.com>
2026-04-27 23:20:56 -0300

    Criando tabela pedito
    Referencia de chave estrangeira id da tabela clientes
    
    Aplicado indice

--- database/01_deploy_ecommerce.sql
+++ database/01_deploy_ecommerce.sql
@@ -13,7 +13,7 @@ create table clientes (
     tipo_cadastro enum('PF', 'PJ') not null default 'PF',
     primary key (id),
     index idx_email (email)     
-)
+);
 
 
 -- 2. Tabela Produtos
@@ -27,7 +27,22 @@ create table produtos (
     categoria varchar(50),
     primary key (id),
     index idx_categoria (categoria)
-)
+);
+
+
+-- 3. Tabela Pedidos  (Cabeçalho)
+CREATE TABLE pedidos (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    cliente_id INT UNSIGNED NOT NULL,
+    data_pedido DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    status ENUM('Aguardando', 'Pago', 'Enviado', 'Entregue', 'Cancelado') DEFAULT 'Aguardando',
+    valor_total DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+    PRIMARY KEY (id),
+    FOREIGN KEY (cliente_id) REFERENCES clientes(id) ON DELETE RESTRICT,
+    INDEX idx_status (status),
+    INDEX idx_data (data_pedido)
+);
+
 
 
 
@@ -36,3 +51,4 @@ create table produtos (
 --Comandos de Verificação
 select * from clientes;
 select * from produtos;
+select * from pedidos;
