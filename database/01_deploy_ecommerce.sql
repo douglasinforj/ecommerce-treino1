22eb22a606c41a027269595551785be0e78fcfcd
Douglas Silva <douglasitpro@gmail.com>
2026-04-27 22:53:44 -0300

    Refact: ajuste no script

--- database/01_deploy_ecommerce.sql
+++ database/01_deploy_ecommerce.sql
@@ -1,16 +1,16 @@
 -- DROP DATABASE IF EXISTS ecommerce_vendas;
 -- CREATE DATABASE ecommerce_vendas;
--- USE ecommercer_vendas;
+USE ecommerce_vendas;
 
 -- 1. Tabela Cliente
 
 create table clientes (
-    id INT UNSIGNED not null auto_increment,
-    nome varchar(100) not null,
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
     email varchar(120) not null unique,
     cpf char(11) not null unique,
     data_cadastro datetime not null default current_timestamp,
     tipo_cadastro enum('PF', 'PJ') not null default 'PF',
     primary key (id),
-    index idx_email (email)     --indice para buscas mais rápidas
+    index idx_email (email)     
 )
