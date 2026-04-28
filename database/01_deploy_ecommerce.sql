73807e5546322e89a0a35da0d121f161a328e64a
Douglas Silva <douglasitpro@gmail.com>
2026-04-28 12:35:10 -0300

    Refact: Ajustando a o campo cpf para até 14 digitos devido a registros de CNPJ

--- database/01_deploy_ecommerce.sql
+++ database/01_deploy_ecommerce.sql
@@ -1,19 +1,22 @@
 -- DROP DATABASE IF EXISTS ecommerce_vendas;
--- CREATE DATABASE ecommerce_vendas;
+#CREATE DATABASE ecommerce_vendas;
 USE ecommerce_vendas;
 
 -- 1. Tabela Cliente
 
 create table clientes (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    nome VARCHAR(100) NOT NULL,
+    nome varchar(100) NOT NULL,
     email varchar(120) not null unique,
     cpf char(11) not null unique,
     data_cadastro datetime not null default current_timestamp,
-    tipo_cadastro enum('PF', 'PJ') not null default 'PF',
+    tipo_cliente enum('PF', 'PJ') not null default 'PF',
     primary key (id),
     index idx_email (email)     
 );
+
+--mudança em coluna (registro de cnpj)
+alter table clientes modify column cpf varchar(14);
 
 
 -- 2. Tabela Produtos
